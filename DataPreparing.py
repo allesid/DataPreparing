0d73304bc0e00294d95f1a288c80100d5efcfdec
Alex Sidoroff <59499133+allesid@users.noreply.github.com>
2021-02-10 00:30:49 +0300

    Update DataPreparing.py
    Не работает

--- DataPreparing.py
+++ DataPreparing.py
@@ -1,4 +1,4 @@
-class DataPreparing()
+class DataPreparing():
     """ Преобразование массива pandas.DataFrame для задач регрессии
         машинного обучения
         Требует import pandas as pd
@@ -10,7 +10,7 @@ class DataPreparing()
         col_y - имя столбца результата регрессии
     """
         
-    def __init__(self, X, col_y)
+    def __init__(self, X, col_y):
         self.X = X
         self.col_y = col_y
         self.y = X[col_y]
@@ -20,20 +20,14 @@ class DataPreparing()
         X = X.drop([col_y])
         for colmn in X.columns:
            if X[colmn].dtype == 'object':
-               return fit_col(self, col, col_y)
+               return self.fit_col(col, col_y)
         
     def fit_col(self, col, col_y):
         """ Преобразование одного столбца X[col] """
-        #col = 'MSZoning'
-        print(X[col].unique())
-        a = X.groupby([col], as_index=False)[col_y].mean()
-        b = a.sort_values([col_y])
-        b.index = pd.Series(range(len(b)))
-        b[col+'_r'] = b.index
+        print("Initial Category data in", col, "feature:", X[col].unique())
+        b = X.groupby([col], as_index=False)[col_y].mean().sort_values([col_y])
+        b[col+'_r'] = pd.Series(range(len(b)))
         X[col] = X[col].replace(list(b[col]), list(b[col+'_r']))
-        X1 = pd.DataFrame()
-        X1[col] = X[col]
-        X1[col_y] = X[col_y]
-        print(X1.corr())
-        print(X1[col].unique())
+        print("Correlation matrix", X[col].corr(X[col_y]))
+        print("Result Category data in", col, "feature:",X[col].unique())
         return X
