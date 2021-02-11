4a41ea2bfa0edbc67c4d032c8cff1161c54bf56e
Alex Sidoroff <59499133+allesid@users.noreply.github.com>
2021-02-11 23:58:31 +0300

    Update DataPreparing.py

--- DataPreparing.py
+++ DataPreparing.py
@@ -1,4 +1,4 @@
-class DataPreparing():
+class DataPreparing(object):
     """ Преобразование массива pandas.DataFrame для задач регрессии
         машинного обучения
         Требует import pandas as pd
@@ -8,26 +8,50 @@ class DataPreparing():
         X - исходный массив типа pandas.DataFrame
         col - имя преобразуемого столбца (признака)
         col_y - имя столбца результата регрессии
+        null_pct - default: 10 - процентное отношение количества отсутствующих 
+                                    данных в столбце к длине столбца
+        nuniqfit_pct - default: 10 - процентное отношение количества уникальных 
+                                    данных в столбце к длине столбца
     """
         
-    def __init__(self, X, col_y):
+    def __init__(self, X, col_y, null_pct=10, nuniqfit_pct=10):
         self.X = X
         self.col_y = col_y
         self.y = X[col_y]
-    
-        """ Преобразование X """
+        self.null_pct = null_pct
+        self.nuniqfit_pct = nuniqfit_pct
     def fit(self, X, col_y):
-        X = X.drop([col_y])
+        """ Преобразование X """
+        X_drop = [col_y]
+        #print(type(X_drop))
         for colmn in X.columns:
-           if X[colmn].dtype == 'object':
-               return self.fit_col(col, col_y)
+            print("--------------------------------------")
+            print(" Feature:", colmn, X[colmn].dtype)
+            if len(X[X[colmn].isna()]) < self.null_pct*len(X)/100:
+                if X[colmn].dtype == 'object':
+                    X[colmn] = X[colmn].fillna('nofeature')
+                    X = self.fit_col(X, colmn, col_y)
+                elif X[colmn].dtype == 'int':
+                    X[colmn] = X[colmn].fillna(0)
+                    print(" Percent of uniquire data:", 100*X[colmn].nunique()/len(X))
+                    if X[colmn].nunique() < self.nuniqfit_pct*len(X)/100:
+                        X = self.fit_col(X, colmn, col_y)
+            else:
+                X_drop.append(colmn)
+                print("   Drop column:", colmn, " Percent of NA data:", 100*len(X[X[colmn].isna()])/len(X))
+        #print(X_drop, type(X_drop))
+        X = X.drop(columns=X_drop)
+        return X
         
-    def fit_col(self, col, col_y):
+    def fit_col(self, X, col, col_y):
         """ Преобразование одного столбца X[col] """
+        print(" Percent of NA data:", 100*len(X[X[col].isna()])/len(X))
         print("Initial Category data in", col, "feature:", X[col].unique())
         b = X.groupby([col], as_index=False)[col_y].mean().sort_values([col_y])
-        b[col+'_r'] = pd.Series(range(len(b)))
+        b.index = pd.Series(range(len(b)))
+        b[col+'_r'] = b.index
+        #print(b)
         X[col] = X[col].replace(list(b[col]), list(b[col+'_r']))
-        print("Correlation matrix", X[col].corr(X[col_y]))
+        print("Correlation matrix", X[col].corr(self.y))
         print("Result Category data in", col, "feature:",X[col].unique())
         return X
